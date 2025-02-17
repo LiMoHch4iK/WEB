199f91c9b6ca55098bc8586905fc02b5709b7fd5
LiMoHch4iK <Glubokova-elizaveta@yandex.com>
2025-02-17 20:11:12 +0300

    Add files via upload

---
+++ Полный поиск.py
@@ -0,0 +1,39 @@
+import pygame
+from lib_search import *
+import sys
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": GEOCODE_MAPS_KEY,
+    "geocode": toponym_to_find,
+    "format": "json"}
+spn, ll = find_ll_spn(geocoder_api_server, geocoder_params)
+
+if not (ll and spn):
+    print(f"{toponym_to_find} - not find")
+    sys.exit(0)
+
+server_address_maps = 'http://static-maps.yandex.ru/v1?'
+map_params = {
+    "ll": ll,
+    "spn": spn,
+    "apikey": STATIC_MAPS_KEY,
+    "pt": "{0},pm2dgl".format(ll)
+}
+
+map_file = make_map(server_address_maps, map_params)
+if map_file:
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+    # os.remove(map_file)
